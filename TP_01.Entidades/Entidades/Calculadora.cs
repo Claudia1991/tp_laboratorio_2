1529f81beb5c632bd5c9114869693b162b8aaf3f
Claudia <serbe_03@hotmail.com>
2020-09-02 20:50:07 -0300

    TP_01: se agrega proyecto entidades

---
+++ TP_01.Entidades/Entidades/Calculadora.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_01.Entidades.Entidades
+{
+    public static class Calculadora
+    {
+        #region Metodos Estaticos
+        public static double Operar(Numero numeroUno, Numero numeroDos, string operador)
+        {
+            return 0;
+        }
+
+        private static string ValidarOperador(char operador)
+        {
+            return string.Empty;
+        }
+        #endregion
+    }
+}
---
+++ TP_01.Entidades/Entidades/Numero.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_01.Entidades.Entidades
+{
+    public class Numero
+    {
+        #region Campos
+        private double numero;
+        #endregion
+
+        #region Propiedades
+        public string SetNumero()
+        {
+            return string.Empty;
+        }
+        #endregion
+
+        #region Metodos Publicos
+        #endregion
+    }
+}
