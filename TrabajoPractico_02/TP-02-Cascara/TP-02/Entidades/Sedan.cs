90628e044766f6bbc52993110c5c53e07665943e
Claudia <serbe_03@hotmail.com>
2020-09-16 20:24:38 -0300

    TP-02: clase Sedan

--- TrabajoPractico_02/TP-02-Cascara/TP-02/Entidades/Sedan.cs
+++ TrabajoPractico_02/TP-02-Cascara/TP-02/Entidades/Sedan.cs
@@ -1,18 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-
-using System.Drawing;
 
 namespace Entidades
 {
-    class Sedan : Vehiculo
+    public class Sedan : Vehiculo
     {
-        public enum ETipo { CuatroPuertas, CincoPuertas }
-        ETipo tipo;
+        #region Enumerados
+        public enum ETipo { CuatroPuertas, CincoPuertas, Monovolumen }
+        #endregion
 
+        #region Campos
+        ETipo tipo;
+        #endregion
+
+        #region Constructores
         /// <summary>
         /// Por defecto, TIPO será Monovolumen
         /// </summary>
@@ -20,34 +21,37 @@ namespace Entidades
         /// <param name="chasis"></param>
         /// <param name="color"></param>
         public Sedan(EMarca marca, string chasis, ConsoleColor color)
-            : base(chasis, marca, color)
+            : base(marca, chasis,  color)
         {
-            tipo = ETipo.CuatroPuertas;
+            tipo = ETipo.Monovolumen;
         }
+        #endregion
 
+        #region Propiedades
         /// <summary>
         /// Los automoviles son medianos
         /// </summary>
-        protected override short Tamanio
+        public override ETamanio Tamanio
         {
             get
             {
-                return this.Tamanio;
+                return ETamanio.Mediano;
             }
         }
+        #endregion
 
+        #region Metodo sobrecargado
         public override sealed string Mostrar()
         {
             StringBuilder sb = new StringBuilder();
 
             sb.AppendLine("SEDAN");
-            sb.AppendLine(this);
-            sb.AppendLine("TAMAÑO : {0}", this.Tamanio);
+            sb.AppendLine(string.Format("TAMAÑO : {0}", this.Tamanio));
             sb.AppendLine("TIPO : " + this.tipo);
-            sb.AppendLine("");
             sb.AppendLine("---------------------");
 
-            return sb;
+            return sb.ToString();
         }
+        #endregion
     }
 }
