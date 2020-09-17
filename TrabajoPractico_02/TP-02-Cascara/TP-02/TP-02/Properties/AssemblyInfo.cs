d46289d188eb0695517f6a599bcf7d71d2e85bdf
Claudia <serbe_03@hotmail.com>
2020-09-17 19:42:05 -0300

    TP-02: versionado, fafafa

--- TrabajoPractico_02/TP-02-Cascara/TP-02/TP-02/Properties/AssemblyInfo.cs
+++ TrabajoPractico_02/TP-02-Cascara/TP-02/TP-02/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("TP-02")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Ejecutable del TP 02")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TP-02")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCompany("Claudia Jara")]
+[assembly: AssemblyProduct("TP 02")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.1.3")]
+[assembly: AssemblyFileVersion("2.0.1.3")]
