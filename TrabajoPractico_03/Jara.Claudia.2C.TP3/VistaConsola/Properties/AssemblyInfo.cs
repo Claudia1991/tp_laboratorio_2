0acf17d4e4280110dfc99ea39a6bfb06489e857b
Claudia <serbe_03@hotmail.com>
2020-10-21 20:20:29 -0300

    TP_03: versionado master

--- TrabajoPractico_03/Jara.Claudia.2C.TP3/VistaConsola/Properties/AssemblyInfo.cs
+++ TrabajoPractico_03/Jara.Claudia.2C.TP3/VistaConsola/Properties/AssemblyInfo.cs
@@ -5,10 +5,10 @@ using System.Runtime.InteropServices;
 // La información general de un ensamblado se controla mediante el siguiente 
 // conjunto de atributos. Cambie estos valores de atributo para modificar la información
 // asociada con un ensamblado.
-[assembly: AssemblyTitle("VistaConsola")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("TP 03 - UTN TSP")]
+[assembly: AssemblyDescription("CLAUDIA JARA")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("CLAUDIA JARA")]
 [assembly: AssemblyProduct("VistaConsola")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Puede especificar todos los valores o utilizar los números de compilación y de revisión predeterminados
 // mediante el carácter '*', como se muestra a continuación:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.3.0.5")]
+[assembly: AssemblyFileVersion("2.3.0.5")]
