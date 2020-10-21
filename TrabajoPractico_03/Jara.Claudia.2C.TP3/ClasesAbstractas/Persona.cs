4ecbaa6a24ec9af6054c0388068d62adeb792956
Claudia <serbe_03@hotmail.com>
2020-10-20 22:50:57 -0300

    TP_03: proyecto clases abstractas

--- TrabajoPractico_03/Jara.Claudia.2C.TP3/ClasesAbstractas/Persona.cs
+++ TrabajoPractico_03/Jara.Claudia.2C.TP3/ClasesAbstractas/Persona.cs
@@ -8,5 +8,96 @@ namespace ClasesAbstractas
 {
     public abstract class Persona
     {
+
+        #region Enumerado
+        public enum ENacionalidad
+        {
+            Argentino,
+            Extranjero
+        }
+        #endregion
+
+        #region Campos
+        private string apellido;
+        private int dni;
+        private ENacionalidad nacionalidad;
+        private string nombre;
+        #endregion
+
+        #region Propiedades
+        public string Apellido
+        {
+            get;
+            set;
+        }
+
+        public int DNI
+        {
+            get;
+            set;
+        }
+
+        public ENacionalidad Nacionalidad
+        {
+            get;
+            set;
+        }
+
+        public string Nombre
+        {
+            get;
+            set;
+        }
+
+        public string StringToDni
+        {
+            get;
+            set;
+        }
+        #endregion
+
+        #region Constructores
+        public Persona()
+        {
+
+        }
+
+        public Persona(string nombre, string apellido, ENacionalidad nacionalidad)
+        {
+
+        }
+
+        public Persona(string nombre, string apellido, int dni, ENacionalidad nacionalidad)
+        {
+
+        }
+
+        public Persona(string nombre, string apellido, string dni, ENacionalidad nacionalidad)
+        {
+
+        }
+        #endregion
+
+        #region Metodos
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+        private int ValidarDni(ENacionalidad nacionalidad, int dato)
+        {
+            return 0;
+        }
+
+        private int ValidarDni(ENacionalidad nacionalidad, string dato)
+        {
+            return 0;
+        }
+
+        private string ValidarNombreApellido(string dato)
+        {
+            return string.Empty;
+        }
+        #endregion
     }
 }
--- TrabajoPractico_03/Jara.Claudia.2C.TP3/ClasesAbstractas/Universitario.cs
+++ TrabajoPractico_03/Jara.Claudia.2C.TP3/ClasesAbstractas/Universitario.cs
@@ -8,5 +8,44 @@ namespace ClasesAbstractas
 {
     public abstract class Universitario : Persona
     {
+        #region Campos
+        private int legajo;
+        #endregion
+
+        #region Constructor
+        public Universitario()
+        {
+
+        }
+
+        public Universitario(int legajo, string nombre, string apellido, string dni, ENacionalidad nacionalidad)
+        {
+
+        }
+        #endregion
+
+        #region Metodos
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        protected string MostrarDatos()
+        {
+            return string.Empty;
+        }
+
+        protected abstract string ParticipaEnClase();
+
+        public static  bool operator ==(Universitario universitarioUno, Universitario universitarioDos)
+        {
+            return true;
+        }
+
+        public static bool operator !=(Universitario universitarioUno, Universitario universitarioDos)
+        {
+            return true;
+        }
+        #endregion
     }
 }
