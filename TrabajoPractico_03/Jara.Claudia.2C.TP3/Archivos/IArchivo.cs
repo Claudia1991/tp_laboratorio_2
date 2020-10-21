b49f4854287b18de823f9da875270c2443dcc6bd
Claudia <serbe_03@hotmail.com>
2020-10-20 22:21:08 -0300

    TP_03: proyecto archivos

--- TrabajoPractico_03/Jara.Claudia.2C.TP3/Archivos/IArchivo.cs
+++ TrabajoPractico_03/Jara.Claudia.2C.TP3/Archivos/IArchivo.cs
@@ -8,5 +8,9 @@ namespace Archivos
 {
     public interface IArchivo<T>
     {
+        #region Metodos
+        bool Guardar(string archivos, T datos);
+        bool Leer(string archivos,out T datos);
+        #endregion
     }
 }
--- TrabajoPractico_03/Jara.Claudia.2C.TP3/Archivos/Xml.cs
+++ TrabajoPractico_03/Jara.Claudia.2C.TP3/Archivos/Xml.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace Archivos
 {
-    public class Xml<T>
+    public class Xml<T> : IArchivo<T>
     {
+        #region Metodos
+        public bool Guardar(string archivos, T datos)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Leer(string archivos, out T datos)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
     }
 }
