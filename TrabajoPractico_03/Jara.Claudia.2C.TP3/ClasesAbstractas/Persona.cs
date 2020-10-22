7d4d26bd0f700278d1ab82fcee2b985ea4739795
Claudia <serbe_03@hotmail.com>
2020-10-22 19:51:26 -0300

    TP_03: entidades abstractas - persona

--- TrabajoPractico_03/Jara.Claudia.2C.TP3/ClasesAbstractas/Persona.cs
+++ TrabajoPractico_03/Jara.Claudia.2C.TP3/ClasesAbstractas/Persona.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Excepciones;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace EntidadesAbstractas
@@ -22,81 +24,166 @@ namespace EntidadesAbstractas
         private int dni;
         private ENacionalidad nacionalidad;
         private string nombre;
+        private Regex regex;
         #endregion
 
         #region Propiedades
         public string Apellido
         {
-            get;
-            set;
+            get
+            {
+                return this.apellido;
+            }
+            set
+            {
+                this.apellido = ValidarNombreApellido(value);
+            }
         }
 
         public int DNI
         {
-            get;
-            set;
+            get
+            {
+                return this.dni;
+            }
+            set
+            {
+                this.dni = ValidarDni(Nacionalidad, value);
+            }
         }
 
         public ENacionalidad Nacionalidad
         {
-            get;
-            set;
+            get
+            {
+                return this.nacionalidad;
+            }
+            set
+            {
+                this.nacionalidad = value;
+            }
         }
 
         public string Nombre
         {
-            get;
-            set;
+            get
+            {
+                return this.nombre;
+            }
+            set
+            {
+                this.nombre = ValidarNombreApellido(value);
+            }
         }
 
         public string StringToDni
         {
-            get;
-            set;
+            get
+            {
+                return DNI.ToString();
+            }
+            set
+            {
+                this.dni = ValidarDni(Nacionalidad, value);
+            }
         }
         #endregion
 
         #region Constructores
         public Persona()
         {
-
         }
 
-        public Persona(string nombre, string apellido, ENacionalidad nacionalidad)
+        public Persona(string nombre, string apellido, ENacionalidad nacionalidad) : this()
         {
-
+            Nombre = nombre;
+            Apellido = apellido;
+            Nacionalidad = nacionalidad;
         }
 
-        public Persona(string nombre, string apellido, int dni, ENacionalidad nacionalidad)
+        public Persona(string nombre, string apellido, int dni, ENacionalidad nacionalidad) : this(nombre, apellido, nacionalidad)
         {
-
+            DNI = dni;
         }
 
-        public Persona(string nombre, string apellido, string dni, ENacionalidad nacionalidad)
+        public Persona(string nombre, string apellido, string dni, ENacionalidad nacionalidad) : this(nombre, apellido, nacionalidad)
         {
-
+            StringToDni = dni;
         }
         #endregion
 
         #region Metodos
         public override string ToString()
         {
-            return base.ToString();
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.AppendFormat("Nombre: {0} - Apellido: {1} - DNI: {2} - Nacionalidad: {3}", Nombre, Apellido, DNI, Nacionalidad);
+            return stringBuilder.ToString();
         }
 
         private int ValidarDni(ENacionalidad nacionalidad, int dato)
         {
-            return 0;
+            int dni = -1;
+            switch (nacionalidad)
+            {
+                case ENacionalidad.Argentino:
+                    if (dato >= 1 && dato <= 89999999)
+                    {
+                        dni = dato;
+                    }
+                    break;
+                case ENacionalidad.Extranjero:
+                    if (dato >= 90000000 && dato <= 99999999)
+                    {
+                        dni = dato;
+                    }
+                    break;
+                default:
+                    throw new NacionalidadInvalidaException();
+            }
+            return dni;
         }
 
         private int ValidarDni(ENacionalidad nacionalidad, string dato)
         {
-            return 0;
+            int dni = 0;
+            int dniParseado;
+            switch (nacionalidad)
+            {
+                case ENacionalidad.Argentino:
+                    regex = new Regex("{0-9}{1,8}");
+                    if (regex.IsMatch(dato) && int.TryParse(dato, out dniParseado) && ValidarDni(nacionalidad, dniParseado) != -1)
+                    {
+                        dni = ValidarDni(nacionalidad, dniParseado);
+                    }
+                    else
+                    {
+                        throw new DniInvalidoException();
+                    }
+                    break;
+                case ENacionalidad.Extranjero:
+                    regex = new Regex("{0-9}{8}");
+                    if (regex.IsMatch(dato) && int.TryParse(dato, out dniParseado) && ValidarDni(nacionalidad, dniParseado) != -1)
+                    {
+                        dni = ValidarDni(nacionalidad, dniParseado);
+                    }
+                    else
+                    {
+                        throw new DniInvalidoException();
+                    }
+                    break;
+            }
+            return dni;
         }
 
         private string ValidarNombreApellido(string dato)
         {
-            return string.Empty;
+            regex = new Regex("{a-zA-Z}{2,20}");
+            string nombre = string.Empty;
+            if (!string.IsNullOrEmpty(dato) && regex.IsMatch(dato))
+            {
+                nombre = dato;
+            }
+            return nombre;
         }
         #endregion
     }
